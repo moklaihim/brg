c43ad3b796a505853b56009a168b7318063121fb
ericong <ericong.kc@gmail.com>
2015-04-04 18:46:02 +0800

    updated

--- www/js/app.js
+++ www/js/app.js
@@ -38,35 +38,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
   // Each tab has its own nav history stack:
 
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
   .state('tab.account', {
     url: '/account',
     views: {
@@ -75,9 +46,19 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         controller: 'AccountCtrl'
       }
     }
-  });
+  })
+
+  .state('tab.user', {
+      url: '/user',
+      views: {
+        'tab-user': {
+          templateUrl: 'templates/tab-user.html',
+          controller: 'UserCtrl'
+        }
+      }
+    });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/account');
 
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -13,8 +13,39 @@ angular.module('starter.controllers', [])
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
+.controller('AccountCtrl', ["$scope", "$ionicPopup", "USer", function($scope, $ionicPopup, User) {
+  $scope.user = User.getUser();
+
+  $scope.login = function (){
+  	User.login($scope.user.email, $scope.user.password, function(res)
+  		{
+  			if (res.id) {
+  				$scope.user = res;
+  			} else {
+  				$ionicPopup.alert({
+  					title: 'Login error!',
+  					template: res.message
+  				});
+  			}
+  		});
   };
-});
+
+  $scope.register = function (){
+  	User.register($scope.user.email, $scope.user.password, function(res)
+		{
+			if (res.id) {
+				$scope.user = res;
+			} else {
+				$ionicPopup.alert({
+					title: 'Register Error!',
+					template: res.message
+				});
+			}
+		});
+  };
+
+  $scope.logout = function () {
+  	User.logout();
+  	$scope.user = {};
+  };
+}]);
