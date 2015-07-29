76b73ef38b5d3f091466dfdb7dc6153092ab7397
ericong <ericong.kc@gmail.com>
2015-07-29 20:39:09 +0800

    resolve error due to offline

--- www/js/controllers/SaleListController.js
+++ www/js/controllers/SaleListController.js
@@ -6,6 +6,7 @@ angular.module('starter.controllers')
 
     $scope.checkStore();
     $scope.users= Users.get_list();
+
     updateSales();
     $scope.showDisOption = false;
     $scope.showSalesView = true;
@@ -55,9 +56,9 @@ angular.module('starter.controllers')
                 $scope.salesClosed = false;
                 // $scope.CloseStyle = {"background-color":"#33cd5f", "border-color":"#28a54c"}
             }
-
         });
     }
+    
 
     $scope.$on('changedDate', updateSales);
 
@@ -66,22 +67,29 @@ angular.module('starter.controllers')
         if(val == true){
             console.log("become Online");
             $scope.users= Users.get_list();
+            console.log("starting updateSales");
             updateSales();
+            console.log("starting online_watch");
             online_watch();
         }
     }, false);
-    
-    var close_watch = $scope.$watch('sales', function(val){
+   
+   
+    var close_watch = $scope.$watch('sales.CLOSED', function(val){
+        
         console.log("Sales changed");
+        
         if($scope.salesClosed){
             console.log("sale is already closed");
             if(!('CLOSED' in $scope.sales)){
                 console.log("Open detected");
                 updateSales();
-            }
+           }
         }else if($scope.sales && 'CLOSED' in $scope.sales){
-            console.log("Close detected");
-            updateSales();
+           console.log("Close detected");
+           updateSales();
         }
-    }, true);
+        
+    }, false);
+
 }])
